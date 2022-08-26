04e2ee00107899af244d8845b0a32056542668fa
ducnd58233 <only4wns@gmail.com>
2022-08-26 11:07:34 +0700

    first commit

---
+++ notes.md
@@ -0,0 +1,11 @@
+```
+npm install --save-dev hardhat
+npx hardhat
+npx hardhat compile
+npx hardhat test
+npx hardhat node
+```
+- deploy
+```
+npx hardhat run .\scripts\deploy.js --network localhost
+```
---
+++ test/sample-test.js
@@ -0,0 +1,18 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
+
+describe("Hello World", function() {
+  const message = "Hello world !!!"
+  const message2 = "Bye world !!!"
+
+  it("Should return message correctly", async function () {
+    const HelloWorld = await ethers.getContractFactory('HelloWorld') // same name with contract name
+    const helloWorld = await HelloWorld.deploy(message) // deploy: call constructor
+    await helloWorld.deployed() // wait for contract finished deploy
+    expect (await helloWorld.printHelloWorld()).to.be.equal(message)
+
+    await helloWorld.updateMessage(message2)
+    expect (await helloWorld.printHelloWorld()).to.be.equal(message2)
+  })
+})
